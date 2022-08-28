87a05e2567694fa79e6f1da94231352f957fe824
Dingo_DCLXVIU <92084391+bitstacc@users.noreply.github.com>
2022-08-28 09:58:30 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#adding our random import
+import random
+
+#making our never ending loop:
+while 1==1:
+    # prompt for a question
+    question = input("Ask the magic 8 ball a question: (press enter to quit)\n")
+
+    if not question: 
+        break
+    print(random.choice(("It is certain", "Outlook good", "You may rely on it", "Ask again later", "Concentrate and ask again",  "Reply hazy, try again", "My reply is no","My sources say no")),"\n")
